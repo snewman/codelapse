59d1474821a7b6a214450556dd7aac06d2de86ab
Sam Newman <sam.newman@gmail.com>
2010-03-15 10:23:49 +0000

    Added tests & todo

---
+++ gitlapse_tests.py
@@ -0,0 +1,20 @@
+import nose
+from nose.tools import *
+from gitlapse import *
+
+def test1():
+    assert False
+
+def test2():
+    assert True
+
+def main():
+    nosemain()
+
+def test_can_create_record():
+    cloc_line = "10,Bourne Shell,56,155,252,3.81,960.12"
+    record = create_record('2009-01-29 16:37:36 +0000', '36b40c7c1168726cc63ad9b7d63d5d209d02da90', cloc_line)
+    assert_equal(record.records['Bourne Shell'], 252)
+
+if __name__ == "__main__":
+    main()
